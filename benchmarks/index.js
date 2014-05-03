d3353d5ccb98861f74093899ca0fa3f95f478134
Fedor Indutny <fedor@indutny.com>
2014-05-04 03:42:10 +0400

    benchmark: add filter support

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -4,23 +4,40 @@ var bn = require('../');
 var bignum = require('bignum');
 var bbignum = require('browserify-bignum');
 
-function compare(op, a, b, c) {
-  var suite = new benchmark.Suite;
+var benchmarks = [];
 
-  console.log('Benchmarking: ' + op);
-  suite
-    .add('bn.js#' + op, a)
-    .add('bignum#' + op, b)
-    .add('browserify-bignum#' + op, c)
-    .on('cycle', function(event) {
-      console.log(String(event.target));
-    })
-    .on('complete', function() {
-      console.log('------------------------');
-      console.log('Fastest is ' + this.filter('fastest').pluck('name'));
-    })
-    .run();
-  console.log('========================');
+function add(op, a, b, c) {
+  benchmarks.push({
+    name: op,
+    start: function start() {
+      var suite = new benchmark.Suite;
+
+      console.log('Benchmarking: ' + op);
+      suite
+        .add('bn.js#' + op, a)
+        .add('bignum#' + op, b)
+        .add('browserify-bignum#' + op, c)
+        .on('cycle', function(event) {
+          console.log(String(event.target));
+        })
+        .on('complete', function() {
+          console.log('------------------------');
+          console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+        })
+        .run();
+      console.log('========================');
+    }
+  });
+}
+
+function start() {
+  var re = process.argv[2] ? new RegExp(process.argv[2], 'i') : /./;
+
+  benchmarks.filter(function(b) {
+    return re.test(b.name);
+  }).forEach(function(b) {
+    b.start();
+  });
 }
 
 var a1 = new bn('012345678901234567890123456789012345678901234567890', 10);
@@ -33,7 +50,7 @@ var as1 = a1.mul(a1);
 var as2 = a2.mul(a2);
 var as3 = a3.mul(a3);
 
-compare('create-10', function() {
+add('create-10', function() {
   new bn('012345678901234567890123456789012345678901234567890', 10);
 }, function() {
   new bignum('012345678901234567890123456789012345678901234567890', 10);
@@ -41,7 +58,7 @@ compare('create-10', function() {
   new bbignum('012345678901234567890123456789012345678901234567890', 10);
 });
 
-compare('create-hex', function() {
+add('create-hex', function() {
   new bn('01234567890abcdef01234567890abcdef01234567890abcdef', 16);
 }, function() {
   new bignum('01234567890abcdef01234567890abcdef01234567890abcdef', 16);
@@ -49,7 +66,7 @@ compare('create-hex', function() {
   new bbignum('01234567890abcdef01234567890abcdef01234567890abcdef', 16);
 });
 
-compare('add', function() {
+add('add', function() {
   a1.add(b1);
 }, function() {
   a2.add(b2);
@@ -57,7 +74,7 @@ compare('add', function() {
   a3.add(b3);
 });
 
-compare('mul', function() {
+add('mul', function() {
   a1.mul(b1);
 }, function() {
   a2.mul(b2);
@@ -65,7 +82,7 @@ compare('mul', function() {
   a3.mul(b3);
 });
 
-compare('sqr', function() {
+add('sqr', function() {
   a1.mul(a1);
 }, function() {
   a2.mul(a2);
@@ -73,10 +90,12 @@ compare('sqr', function() {
   a3.mul(a3);
 });
 
-compare('div', function() {
+add('div', function() {
   as1.div(a1);
 }, function() {
   as2.div(a2);
 }, function() {
   as3.div(a3);
 });
+
+start();
