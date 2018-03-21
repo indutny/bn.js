c097725408749ef24a643687a93fe6fa6be29897
Valentin Sundermann <me@vsund.de>
2018-03-21 22:41:09 +0100

    Add test frame for types

---
+++ test/typescript-test.ts
@@ -0,0 +1,9 @@
+import { BN } from '../';
+
+
+console.log('Started to test TypeScript declarations with executing "test/typescript-test.ts"\n');
+
+// TODO
+
+console.log('\nFinished with testing TypeScript declarations');
+console.log('If you see this, everything might have worked');
