af4a8fac6e8c63dd0a8c5f74b38189f982edcec7
noelyoo <innovation.noel@gmail.com>
2018-09-18 23:49:30 +0900

    Refactor buffer constructor

--- test/pummel/dh-group-test.js
+++ test/pummel/dh-group-test.js
@@ -16,7 +16,7 @@ describe('BN.js/Slow DH test', function () {
       var mont = BN.red(new BN(group.prime, 16));
       var priv = new BN(group.priv, 16);
       var multed = base.toRed(mont).redPow(priv).fromRed();
-      var actual = new Buffer(multed.toArray());
+      var actual = Buffer.from(multed.toArray());
       assert.equal(actual.toString('hex'), group.pub);
     });
   });
